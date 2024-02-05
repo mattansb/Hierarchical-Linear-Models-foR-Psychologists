b4c86e6438ec7297ad480982b34d381a99e58f73
Mattan S. Ben-Shachar <mattansb@msbstats.info>
2024-02-05 12:50:17 +0200

    Update 02 R matrix.R

--- 04 Growth Models/02 R matrix.R
+++ 04 Growth Models/02 R matrix.R
@@ -1,39 +1,51 @@
-library(nlme)
-library(glmmTMB)
 library(ggplot2)
 
+library(nlme)
+library(glmmTMB)
+
+
 data <- read.csv("Hoffman_Chap.6-Quadratic growth model.csv")
+data$PersonID <- factor(data$PersonID)
 head(data)
 
+
+set.seed(42)
 data |> 
-  subset(PersonID %in% sample(PersonID, 16)) |> 
+  subset(PersonID %in% sample(levels(PersonID), 16)) |> 
   ggplot(aes(session, rt)) +
   geom_point() +
-  stat_smooth(method = "lm") +
+  stat_smooth(method = "lm", se = FALSE) +
   facet_wrap(~ PersonID, scales = "free") +
-  theme_void()
+  theme_classic()
 # Can you see the auto-correlation?
 
 
 # Standard model ---------------------------
 
-m1a <- lme(rt ~ session, data = data,
-           random = ~ session | PersonID)
+m1a <- lme(rt ~ session,
+           random = ~ session | PersonID,
+           method = "REML",
+           data = data)
 summary(m1a)
 
 # Or:
-m1b <- glmmTMB(rt ~ session + (session | PersonID), 
+m1b <- glmmTMB(rt ~ session + (session | PersonID),
+               REML = TRUE,
                data = data)
 summary(m1b)
 
 
 # Autocorrelation ---------------------------
 
-m2a <- lme(rt ~ session, data = data,
+m2a <- lme(rt ~ session, 
            random = ~ session | PersonID,
-           correlation = corAR1(form = ~ session | PersonID))
-summary(m2a)
+           correlation = corAR1(form = ~ session | PersonID),
+           method = "REML",
+           data = data)
 anova(m2a, m1a)
+
+summary(m2a)
+
 
 # AR1 parameter:
 m2a[["modelStruct"]][["corStruct"]]
@@ -45,6 +57,8 @@ m2a[["modelStruct"]][["corStruct"]]
 # https://cran.r-project.org/web/packages/glmmTMB/vignettes/covstruct.html
 m2b <- glmmTMB(rt ~ session + (session | PersonID) +
                  ar1(0 + factor(session) | PersonID),
+               dispformula = ~ 0, 
+               REML = TRUE,
                data = data)
 summary(m2b)
 VarCorr(m2b)

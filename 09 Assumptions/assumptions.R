d8d9e2c3b413310b3729f0f13c75bf1597bdc688
Mattan S. Ben-Shachar <mattansb@msbstats.info>
2025-01-27 10:37:08 +0200

    Update assumptions.R

--- 09 Assumptions/assumptions.R
+++ 09 Assumptions/assumptions.R
@@ -12,7 +12,7 @@ library(performance)
 
 data("sleepstudy", package = "lme4")
 
-mod <- lmer(Reaction ~ Days + (Days | Subject), 
+mod <- lmer(Reaction ~ Days + (Days | Subject),
             data = sleepstudy)
 
 
@@ -33,16 +33,16 @@ check_heteroscedasticity(mod) |> plot()
 # By grouping variable:
 sleepstudy$resid <- residuals(mod)
 
-sleepstudy |> 
+sleepstudy |>
   mutate(
     Subject = fct_reorder(Subject, resid ^ 2)
-  ) |> 
-  ggplot(aes(Subject, resid ^ 2)) + 
-  geom_point() + 
-  stat_summary(geom = "point", size = 3, color = "red") + 
-  scale_y_continuous(name = expression((y[i] - hat(y)[i]) ^ 2), 
-                     labels = scales::label_comma(),
-                     transform = scales::transform_sqrt()) + 
+  ) |>
+  ggplot(aes(Subject, resid ^ 2)) +
+  geom_point() +
+  stat_summary(geom = "point", size = 3, color = "red") +
+  scale_y_continuous(name = expression((y[i] - hat(y)[i]) ^ 2),
+                     labels = scales::label_comma()) +
+  coord_trans(y = scales::transform_sqrt()) +
   theme_bw()
 # It looks like some subjects have more within-person (level 1) variance (334,
 # 308) than others...
@@ -66,17 +66,14 @@ sleepstudy |>
 ## +2. Random effects are normally distributed -------------
 
 check_model(mod, check = "reqq")
-# Produces QQ-plots for random effects. 
+# Produces QQ-plots for random effects.
 # We expect the dots to follow the green line (in this case, they do).
 
 
 # Dealing violations:
-# - Probably not so impactful (https://doi.org/10.1007/s11135-018-0802-x)... 
+# - Probably not so impactful (https://doi.org/10.1007/s11135-018-0802-x)...
 
 
 ## Check all... ----------------------
 
 check_model(mod)
-
-
-
